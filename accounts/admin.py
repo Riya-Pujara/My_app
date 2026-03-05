852987e13ac2d66127980eb29a61eb99a110bbf8
rushika <you@example.com>
2026-03-05 21:53:52 +0530

    register all models in admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, seekerdb, employerdb, Job, Application, Notification
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(seekerdb)
+admin.site.register(employerdb)
+admin.site.register(Job)
+admin.site.register(Notification)
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('seeker', 'job', 'status', 'applied_at')
+    list_filter = ('status', 'applied_at')
+    search_fields = ('seeker__full_name', 'job__job_role')
