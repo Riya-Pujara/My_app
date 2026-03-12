b8f6ed57cf2704ba7b449c67d880c7cc5f51ed04
Rushika Virani <24ceuog166@ddu.ac.in>
2026-03-12 23:45:52 +0530

    add urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('all_jobs/', views.view_all_jobs, name='view_all_jobs'),
     path('delete_job/<int:job_id>/', views.delete_job, name='delete_job'),
     path('admin/', admin.site.urls),
-    path('suspend_user/<int:user_id>/', views.suspend_user, name='suspend_user'),
+    path('suspend-user/<int:user_id>/', views.suspend_user, name='suspend_user'),
+    path('activate-user/<int:user_id>/', views.activate_user, name='activate_user'),
 ]
